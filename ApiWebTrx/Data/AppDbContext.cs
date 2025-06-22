827f155575f78b444ca7bdf62d9e9ad97531d783
Patricia Souza <patricia.sz.br@gmail.com>
2025-06-21 21:47:10 -0300

    Config Entity DbContext

---
+++ ApiWebTrx/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiWebTrx.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWebTrx.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options ) : base(options)
+        { }
+            
+        public DbSet<ProdutoModel> Produtos { get; set; }   
+    }
+}
---
+++ ApiWebTrx/Models/ProdutoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiWebTrx.Models
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public string QuantidadeEstoque { get; set; } = string.Empty;
+        public string CodigoDeBarras { get; set; } = string.Empty;
+        public string Marca { get; set; } = string.Empty;
+    }
+}
